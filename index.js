8c7e3979a303636f7d5b17b4a2dea696ea8cea69
Ethan <ethansagin@gmail.com>
2019-01-29 20:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(currentLine) {
   if(currentLine.length === 0) {
     return 'There is nobody waiting to be served!';
   } else {
-    return `Currently serving ${currentLine}.` &&   currentLine.shift();
+    return `Currently serving ${currentLine[0]}.` &&   currentLine.shift();
   }
 }
 
