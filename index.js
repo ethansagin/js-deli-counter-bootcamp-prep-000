ceaeaa07e119bdbbd6f286da50d947502542ff1f
Ethan <ethansagin@gmail.com>
2019-01-29 20:03:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var test = [1, 2, 3]
+
+function takeANumber(currentLine, newPerson) {
+  currentLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`;
+}
+
+function nowServing(currentLine) {
+  if(currentLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    return 'Currently serving ' + currentLine[0] &&   currentLine.shift();
+  }
+}
+
+function currentLine(current) {
+  for(let i = 0; i < current.length; i++) {
+    return `${i++}. ${current[i]}, `;
+  }
+}
+
+console.log(currentLine(test))
